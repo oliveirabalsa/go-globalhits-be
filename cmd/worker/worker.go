0e78f853ff3ded7a0dcbd3235e937c3c6c34e8ed
Leonardo <oliveirabalsa2@gmail.com>
2024-03-02 00:22:21 -0300

    feat: change worker init

--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -5,14 +5,14 @@ import (
 	"log"
 
 	"github.com/oliveirabalsa/go-globalhitss-be/app/model"
+	"github.com/oliveirabalsa/go-globalhitss-be/app/repository"
+	"github.com/oliveirabalsa/go-globalhitss-be/app/usecase"
+	"github.com/oliveirabalsa/go-globalhitss-be/config"
 	"github.com/oliveirabalsa/go-globalhitss-be/queue"
 )
 
 func main() {
-	conn, ch, err := queue.NewRabbitMQ()
-	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
-	}
+	ch, conn, db := config.InitServices()
 	defer conn.Close()
 	defer ch.Close()
 
@@ -30,11 +30,16 @@ func main() {
 		log.Fatalf("Failed to register a consumer: %v", err)
 	}
 
+	clientRepo := repository.NewClientRepository(db)
+
+	clientUsecase := usecase.NewClientUseCase(*clientRepo)
+
 	forever := make(chan bool)
 
 	go func() {
 		for d := range msgs {
 			var message queue.Message
+
 			err := json.Unmarshal(d.Body, &message)
 			if err != nil {
 				log.Printf("Failed to unmarshal message: %v", err)
@@ -50,7 +55,11 @@ func main() {
 					continue
 				}
 
-				log.Printf("Received message to create client: %+v", client)
+				_, err = clientUsecase.CreateClient(&client)
+				if err != nil {
+					log.Printf("Failed to create client: %v", err)
+					continue
+				}
 			}
 		}
 	}()
