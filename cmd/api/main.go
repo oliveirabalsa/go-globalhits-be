c2ae0b0b18b4a3e3d6da45b15f031ef90faccf1f
Leonardo <oliveirabalsa2@gmail.com>
2024-03-02 13:26:11 -0300

    feat: add swagger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,7 +8,10 @@ import (
 	"github.com/oliveirabalsa/go-globalhitss-be/app/handler"
 	"github.com/oliveirabalsa/go-globalhitss-be/app/repository"
 	"github.com/oliveirabalsa/go-globalhitss-be/app/usecase"
+	// _ "github.com/oliveirabalsa/go-globalhitss-be/cmd/api/docs"
 	"github.com/oliveirabalsa/go-globalhitss-be/config"
+	_ "github.com/oliveirabalsa/go-globalhitss-be/docs"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
 func main() {
@@ -17,20 +20,19 @@ func main() {
 	defer ch.Close()
 
 	clientRepo := repository.NewClientRepository(db)
-
 	clientUsecase := usecase.NewClientUseCase(*clientRepo, ch, "globalhitss")
-
-	clientHandler := handler.ClientHandler{
-		ClientUsecase: *clientUsecase,
-	}
+	clientHandler := handler.ClientHandler{ClientUsecase: *clientUsecase}
 
 	r := chi.NewRouter()
+	r.Mount("/swagger/", httpSwagger.WrapHandler)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world!"))
+		w.Write([]byte("OK"))
 	})
+
 	r.Post("/clients", clientHandler.CreateClient)
 	r.Get("/clients", clientHandler.GetClients)
-	// Add other routes as needed
+	r.Patch("/clients/{id}", clientHandler.UpdateClient)
+	r.Delete("/clients/{id}", clientHandler.DeleteClient)
 
 	log.Println("Starting server on :8082...")
 	if err := http.ListenAndServe(":8082", r); err != nil {
--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"log"
 
-	"github.com/oliveirabalsa/go-globalhitss-be/app/model"
+	"github.com/oliveirabalsa/go-globalhitss-be/app/handler"
 	"github.com/oliveirabalsa/go-globalhitss-be/app/repository"
 	"github.com/oliveirabalsa/go-globalhitss-be/config"
 	"github.com/oliveirabalsa/go-globalhitss-be/queue"
@@ -15,52 +15,34 @@ func main() {
 	defer conn.Close()
 	defer ch.Close()
 
-	queueName := "globalhitss"
-	msgs, err := ch.Consume(
-		queueName,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
+	msgs, err := config.SetupConsumer(ch)
+
 	if err != nil {
 		log.Fatalf("Failed to register a consumer: %v", err)
 	}
 
 	clientRepo := repository.NewClientRepository(db)
-
 	forever := make(chan bool)
 
 	go func() {
 		for d := range msgs {
 			var message queue.Message
-
 			err := json.Unmarshal(d.Body, &message)
 			if err != nil {
 				log.Printf("Failed to unmarshal message: %v", err)
 				continue
 			}
-
 			switch message.Action {
 			case "create_client":
-				var client model.Client
-				err := json.Unmarshal(message.Data, &client)
-				if err != nil {
-					log.Printf("Failed to unmarshal client data: %v", err)
-					continue
-				}
-
-				_, err = clientRepo.Create(&client)
-				if err != nil {
-					log.Printf("Failed to create client: %v", err)
-					continue
-				}
+				handler.CreateClientHandler(clientRepo, message)
+			case "update_client":
+				handler.UpdateClientHandler(clientRepo, message)
+			case "delete_client":
+				handler.DeleteClientHandler(clientRepo, message)
 			}
 		}
 	}()
 
-	log.Printf("Worker running. To exit press CTRL+C")
+	log.Printf("Starting worker...")
 	<-forever
 }
