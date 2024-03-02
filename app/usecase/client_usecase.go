093882b802e8fabcea10df60c80e3ea32e698998
Leonardo <oliveirabalsa2@gmail.com>
2024-03-02 13:26:05 -0300

    feat: add delete use case

--- app/usecase/client_usecase.go
+++ app/usecase/client_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"encoding/json"
 
+	"github.com/google/uuid"
 	"github.com/oliveirabalsa/go-globalhitss-be/app/model"
 	"github.com/oliveirabalsa/go-globalhitss-be/app/repository"
 	"github.com/oliveirabalsa/go-globalhitss-be/queue"
@@ -40,6 +41,41 @@ func (uc *ClientUsecase) CreateClient(client *model.Client) (string, error) {
 	return "Your data has been received and is being processed.", nil
 }
 
+func (uc *ClientUsecase) UpdateClient(clientId uuid.UUID, client *model.Client) (string, error) {
+	client.ID = clientId
+	clientData, err := json.Marshal(client)
+	if err != nil {
+		return "", err
+
+	}
+
+	message := &queue.Message{
+		Action: "update_client",
+		Data:   clientData,
+	}
+	err = queue.PublishMessage(uc.QueueChannel, uc.QueueName, message)
+	if err != nil {
+		return "", err
+	}
+	return "Your data has been received and is being processed.", nil
+}
+
 func (uc *ClientUsecase) GetClients() ([]*model.Client, error) {
 	return uc.ClientRepo.GetAll()
+}
+
+func (uc *ClientUsecase) DeleteClient(clientId uuid.UUID) (string, error) {
+	if err := uc.ClientRepo.ClientExists(clientId); err != nil {
+		return "", err
+	}
+
+	message := &queue.Message{
+		Action: "delete_client",
+		Data:   []byte(clientId.String()),
+	}
+	if err := queue.PublishMessage(uc.QueueChannel, uc.QueueName, message); err != nil {
+		return "", err
+	}
+
+	return "Your data has been received and is being processed.", nil
 }
