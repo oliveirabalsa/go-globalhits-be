1c6ea20663e8b6934d08d01b11749d2fdd0d1ed1
Leonardo <oliveirabalsa2@gmail.com>
2024-03-02 13:25:54 -0300

    feat: add soft delete

--- app/repository/client_repository.go
+++ app/repository/client_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/google/uuid"
 	"github.com/oliveirabalsa/go-globalhitss-be/app/model"
 	"gorm.io/gorm"
 )
@@ -17,7 +18,7 @@ func NewClientRepository(db *gorm.DB) *ClientRepository {
 
 func (r *ClientRepository) GetAll() ([]*model.Client, error) {
 	var clients []*model.Client
-	if err := r.DB.Find(&clients).Error; err != nil {
+	if err := r.DB.Where("active = ?", true).Find(&clients).Error; err != nil {
 		return nil, err
 	}
 	return clients, nil
@@ -28,4 +29,35 @@ func (r *ClientRepository) Create(client *model.Client) (*model.Client, error) {
 		return nil, err
 	}
 	return client, nil
+}
+
+func (r *ClientRepository) Update(client *model.Client) (*model.Client, error) {
+	if err := r.ClientExists(client.ID); err != nil {
+		return nil, err
+	}
+
+	if err := r.DB.Model(&client).Updates(client).Error; err != nil {
+		return nil, err
+	}
+
+	return client, nil
+
+}
+
+func (r *ClientRepository) DeleteClient(clientId uuid.UUID) error {
+	if err := r.ClientExists(clientId); err != nil {
+		return err
+	}
+	if err := r.DB.Model(&model.Client{}).Where("id = ?", clientId).Update("active", false).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *ClientRepository) ClientExists(clientId uuid.UUID) error {
+	var client model.Client
+	if err := r.DB.Where("id = ? AND active = ?", clientId, true).First(&client).Error; err != nil {
+		return err
+	}
+	return nil
 }
