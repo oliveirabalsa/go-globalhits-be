6bd033aa4a070a30c04f25e574bf6fb215c9009f
Leonardo <oliveirabalsa2@gmail.com>
2024-03-03 06:40:45 -0300

    feat: add env vars to postgres

--- db/postgres.go
+++ db/postgres.go
@@ -1,17 +1,29 @@
 package db
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/oliveirabalsa/go-globalhitss-be/app/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewPostgresDB() (*gorm.DB, error) {
-	dsn := "host=localhost user=globalhitss password=globalhitss dbname=globalhitss port=5432 sslmode=disable TimeZone=UTC"
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		host, user, password, dbname, port)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
+
 	db.AutoMigrate(&model.Client{})
 	return db, nil
 }
