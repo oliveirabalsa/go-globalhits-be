9534ae5c9cd900e96d3ef159d9fe3f179b673945
Leonardo <oliveirabalsa2@gmail.com>
2024-03-03 06:41:18 -0300

    feat: add audit to model

--- app/model/client.go
+++ app/model/client.go
@@ -7,17 +7,20 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
+	"github.com/oliveirabalsa/go-globalhitss-be/app/utils"
 )
 
 type Client struct {
-	ID          uuid.UUID `gorm:"primaryKey" json:"id"`
-	Name        string    `json:"name" validate:"required"`
-	LastName    string    `json:"last_name" validate:"required"`
-	Contact     string    `json:"contact"`
-	Address     string    `json:"address"`
-	DateOfBirth string    `json:"date_of_birth" validate:"required,date_of_birth"`
-	CPF         string    `json:"cpf" validate:"required,cpf"`
-	Active      bool      `gorm:"default:true" json:"active"`
+	ID          uuid.UUID `gorm:"primaryKey" json:"id" swaggerignore:"true"`
+	Name        string    `json:"name" validate:"required" example:"Fulano"`
+	LastName    string    `json:"last_name" validate:"required" example:"da Silva"`
+	Contact     string    `json:"contact" example:"fulano.dasilva@example.com"`
+	Address     string    `json:"address" example:"Rua x, bairro da rua, cidade/estado"`
+	DateOfBirth string    `json:"date_of_birth" validate:"required,date_of_birth" example:"12/12/1912"`
+	CPF         string    `json:"cpf" validate:"required,cpf" example:"123.456.789-00"`
+	Active      bool      `gorm:"default:true" json:"active" swaggerignore:"true"`
+	CreatedAt   time.Time `gorm:"created_at" json:"createdAt"`
+	UpdatedAt   time.Time `gorm:"updated_at" json:"updatedAt"`
 }
 
 func (Client) TableName() string {
@@ -55,4 +58,46 @@ func (c *Client) Validate() error {
 	}
 
 	return nil
+}
+
+func (c *Client) EncryptSensitiveData() {
+	if c.Name != "" {
+		c.Name = encryption.Encrypt(c.Name)
+	}
+	if c.LastName != "" {
+		c.LastName = encryption.Encrypt(c.LastName)
+	}
+	if c.Contact != "" {
+		c.Contact = encryption.Encrypt(c.Contact)
+	}
+	if c.Address != "" {
+		c.Address = encryption.Encrypt(c.Address)
+	}
+	if c.DateOfBirth != "" {
+		c.DateOfBirth = encryption.Encrypt(c.DateOfBirth)
+	}
+	if c.CPF != "" {
+		c.CPF = encryption.Encrypt(c.CPF)
+	}
+}
+
+func (c *Client) DecryptSensitiveData() {
+	if c.Name != "" {
+		c.Name = encryption.Decrypt(c.Name)
+	}
+	if c.LastName != "" {
+		c.LastName = encryption.Decrypt(c.LastName)
+	}
+	if c.Contact != "" {
+		c.Contact = encryption.Decrypt(c.Contact)
+	}
+	if c.Address != "" {
+		c.Address = encryption.Decrypt(c.Address)
+	}
+	if c.DateOfBirth != "" {
+		c.DateOfBirth = encryption.Decrypt(c.DateOfBirth)
+	}
+	if c.CPF != "" {
+		c.CPF = encryption.Decrypt(c.CPF)
+	}
 }
