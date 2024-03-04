e9315fdbe28f573f4d121ffca3c6995c1bacbc61
Leonardo <oliveirabalsa2@gmail.com>
2024-03-03 23:55:11 -0300

    feat: add transactions

--- app/repository/client_repository.go
+++ app/repository/client_repository.go
@@ -22,50 +22,79 @@ func (r *ClientRepository) GetAll(page int, pageSize int) ([]*model.Client, int,
 	var clients []*model.Client
 	var totalRecords int64
 
-	if err := r.DB.Model(&model.Client{}).Where("active = ?", true).Count(&totalRecords).Error; err != nil {
-		return nil, 0, err
-	}
+	err := WithTransaction(r.DB, func(tx *gorm.DB) error {
+		if err := tx.Model(&model.Client{}).Where("active = ?", true).Count(&totalRecords).Error; err != nil {
+			return err
+		}
 
-	if err := r.DB.Where("active = ?", true).Offset((page - 1) * pageSize).Limit(pageSize).Find(&clients).Error; err != nil {
-		return nil, 0, err
-	}
+		if err := tx.Where("active = ?", true).Offset((page - 1) * pageSize).Limit(pageSize).Find(&clients).Error; err != nil {
+			return err
+		}
+
+		return nil
+	})
 
 	totalPages := int(math.Ceil(float64(totalRecords) / float64(pageSize)))
 
-	return clients, totalPages, nil
+	return clients, totalPages, err
 }
 
 func (r *ClientRepository) GetByID(clientID uuid.UUID) (*model.Client, error) {
 	var client model.Client
-	if err := r.DB.Where("id = ? and active = true", clientID).First(&client).Error; err != nil {
-		return nil, err
-	}
-	return &client, nil
+	err := WithTransaction(r.DB, func(tx *gorm.DB) error {
+		return tx.Where("id = ? and active = true", clientID).First(&client).Error
+	})
+
+	return &client, err
 }
 
 func (r *ClientRepository) Create(client *model.Client) (*model.Client, error) {
-	if err := r.DB.Create(client).Error; err != nil {
-		return nil, err
-	}
-	return client, nil
+	err := WithTransaction(r.DB, func(tx *gorm.DB) error {
+		return tx.Create(client).Error
+	})
+
+	return client, err
 }
 
 func (r *ClientRepository) Update(client *model.Client) (*model.Client, error) {
-	if err := r.DB.Model(&client).Updates(client).Error; err != nil {
-		return nil, err
-	}
+	err := WithTransaction(r.DB, func(tx *gorm.DB) error {
+		return tx.Model(&client).Updates(client).Error
+	})
 
-	return client, nil
-
+	return client, err
 }
 
 func (r *ClientRepository) DeleteClient(clientId uuid.UUID) error {
-	var client model.Client
-	if err := r.DB.Where("id = ? AND active = ?", clientId, true).First(&client).Error; err != nil {
+	err := WithTransaction(r.DB, func(tx *gorm.DB) error {
+		var client model.Client
+		if err := tx.Where("id = ? AND active = ?", clientId, true).First(&client).Error; err != nil {
+			return err
+		}
+		if err := tx.Model(&model.Client{}).Where("id = ?", clientId).Update("active", false).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return err
+}
+
+func WithTransaction(db *gorm.DB, fn func(tx *gorm.DB) error) error {
+	tx := db.Begin()
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := fn(tx); err != nil {
+		tx.Rollback()
 		return err
 	}
-	if err := r.DB.Model(&model.Client{}).Where("id = ?", clientId).Update("active", false).Error; err != nil {
-		return err
-	}
-	return nil
+
+	return tx.Commit().Error
 }
